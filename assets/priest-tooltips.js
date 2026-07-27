ec2d38aa97bd72f36b88fa405642e517f74f3f8e
leblackstock <leblackstock@yahoo.com>
2026-07-26 21:32:23 -0400

    Add fresh-80 Priest and Hunter guides

---
+++ assets/priest-tooltips.js
@@ -0,0 +1,179 @@
+/* Generated from the verified class entity registry. Do not hand-edit this file. */
+(function () {
+  "use strict";
+
+  const rows = [{"type":"item","id":41401,"names":["Insightful Earthsiege Diamond"],"icon":"inv_jewelcrafting_shadowspirit_02","category":"gem"},{"type":"item","id":41285,"names":["Chaotic Skyflare Diamond"],"icon":"inv_jewelcrafting_shadowspirit_02","category":"gem"},{"type":"item","id":40113,"names":["Runed Cardinal Ruby"],"icon":"inv_jewelcrafting_gem_37","category":"gem"},{"type":"item","id":40151,"names":["Luminous Ametrine"],"icon":"inv_jewelcrafting_gem_39","category":"gem"},{"type":"item","id":40128,"names":["Quick King's Amber","Quick King’s Amber"],"icon":"inv_jewelcrafting_gem_38","category":"gem"},{"type":"item","id":40133,"names":["Purified Dreadstone"],"icon":"inv_jewelcrafting_gem_40","category":"gem"},{"type":"item","id":40120,"names":["Sparkling Majestic Zircon"],"icon":"inv_jewelcrafting_gem_42","category":"gem"},{"type":"item","id":40155,"names":["Reckless Ametrine"],"icon":"inv_jewelcrafting_gem_39","category":"gem"},{"type":"item","id":40153,"names":["Veiled Ametrine"],"icon":"inv_jewelcrafting_gem_39","category":"gem"},{"type":"item","id":40125,"names":["Rigid King's Amber","Rigid King’s Amber"],"icon":"inv_jewelcrafting_gem_38","category":"gem"},{"type":"item","id":49110,"names":["Nightmare Tear"],"icon":"inv_misc_gem_pearl_12","category":"gem"},{"type":"item","id":44159,"names":["Arcanum of Burning Mysteries"],"icon":"spell_fire_masterofelements","category":"enchant-item"},{"type":"item","id":44135,"names":["Greater Inscription of the Storm"],"icon":"inv_inscription_tradeskill01","category":"enchant-item"},{"type":"item","id":41611,"names":["Eternal Belt Buckle"],"icon":"inv_belt_36","category":"enchant-item"},{"type":"item","id":46376,"names":["Flask of the Frost Wyrm"],"icon":"inv_alchemy_endlessflask_05","category":"consumable"},{"type":"item","id":40212,"names":["Potion of Wild Magic"],"icon":"inv_alchemy_elixir_01","category":"consumable"},{"type":"item","id":33448,"names":["Runic Mana Potion"],"icon":"inv_alchemy_elixir_02","category":"consumable"},{"type":"item","id":43015,"names":["Fish Feast"],"icon":"inv_misc_fish_52","category":"consumable"},{"type":"item","id":34767,"names":["Firecracker Salmon"],"icon":"inv_misc_food_47","category":"consumable"},{"type":"item","id":42396,"names":["Glyph of Circle of Healing"],"icon":"inv_glyph_majorpriest","category":"glyph"},{"type":"item","id":42409,"names":["Glyph of Prayer of Healing"],"icon":"inv_glyph_majorpriest","category":"glyph"},{"type":"item","id":45755,"names":["Glyph of Guardian Spirit"],"icon":"inv_glyph_majorpriest","category":"glyph"},{"type":"item","id":42400,"names":["Glyph of Flash Heal"],"icon":"inv_glyph_majorpriest","category":"glyph"},{"type":"item","id":42407,"names":["Glyph of Shadow"],"icon":"inv_glyph_majorpriest","category":"glyph"},{"type":"item","id":42415,"names":["Glyph of Mind Flay"],"icon":"inv_glyph_majorpriest","category":"glyph"},{"type":"item","id":45753,"names":["Glyph of Dispersion"],"icon":"inv_glyph_majorpriest","category":"glyph"},{"type":"item","id":42414,"names":["Glyph of Shadow Word: Death"],"icon":"inv_glyph_majorpriest","category":"glyph"},{"type":"item","id":43374,"names":["Glyph of Shadowfiend"],"icon":"inv_glyph_minorpriest","category":"glyph"},{"type":"item","id":43371,"names":["Glyph of Fortitude"],"icon":"inv_glyph_minorpriest","category":"glyph"},{"type":"item","id":43370,"names":["Glyph of Levitate"],"icon":"inv_glyph_minorpriest","category":"glyph"},{"type":"item","id":43372,"names":["Glyph of Shadow Protection"],"icon":"inv_glyph_minorpriest","category":"glyph"},{"type":"item","id":47215,"names":["Tears of the Vanquished"],"icon":"inv_jewelry_talisman_07","category":"gear"},{"type":"item","id":37360,"names":["Staff of Draconic Combat"],"icon":"inv_staff_77","category":"gear"},{"type":"item","id":37169,"names":["War Mace of Unrequited Love"],"icon":"inv_mace_72","category":"gear"},{"type":"item","id":41384,"names":["Titansteel Guardian"],"icon":"inv_mace_82","category":"gear"},{"type":"item","id":44255,"names":["Darkmoon Card: Greatness"],"icon":"inv_inscription_tarotgreatness","category":"gear"},{"type":"item","id":44210,"names":["Faces of Doom"],"icon":"inv_misc_book_11","category":"gear"},{"type":"item","id":47585,"names":["Bejeweled Wizard's Bracers","Bejeweled Wizard’s Bracers"],"icon":"inv_bracer_39","category":"gear"},{"type":"item","id":49892,"names":["Lightweave Leggings"],"icon":"inv_pants_cloth_37","category":"gear"},{"type":"item","id":49893,"names":["Sandals of Consecration"],"icon":"inv_boots_cloth_21","category":"gear"},{"type":"item","id":48724,"names":["Talisman of Resurgence"],"icon":"inv_jewelry_talisman_10","category":"gear"},{"type":"item","id":50358,"names":["Purified Lunar Dust"],"icon":"inv_misc_ammo_arrow_02","category":"gear"},{"type":"item","id":47059,"names":["Solace of the Defeated"],"icon":"inv_misc_idol_03","category":"gear"},{"type":"item","id":49968,"names":["Frozen Bonespike"],"icon":"inv_weapon_shortblade_99","category":"gear"},{"type":"item","id":50359,"names":["Althor's Abacus","Althor’s Abacus"],"icon":"inv_misc_enggizmos_18","category":"gear"},{"type":"item","id":50428,"names":["Royal Scepter of Terenas II"],"icon":"inv_mace_108","category":"gear"},{"type":"item","id":50731,"names":["Archus, Greatstaff of Antonidas"],"icon":"inv_staff_109","category":"gear"},{"type":"item","id":47213,"names":["Abyssal Rune"],"icon":"inv_misc_rune_09","category":"gear"},{"type":"item","id":50259,"names":["Nevermelting Ice Crystal"],"icon":"inv_misc_gem_ice_01","category":"gear"},{"type":"item","id":40682,"names":["Sundial of the Exiled"],"icon":"inv_jewelry_talisman_05","category":"gear"},{"type":"item","id":40432,"names":["Illustration of the Dragon Soul"],"icon":"inv_offhand_naxxramas_04","category":"gear"},{"type":"item","id":47603,"names":["Merlin's Robe","Merlin’s Robe"],"icon":"inv_chest_cloth_74","category":"gear"},{"type":"item","id":49890,"names":["Deathfrost Boots"],"icon":"inv_boots_cloth_19","category":"gear"},{"type":"item","id":49891,"names":["Leggings of Woven Death"],"icon":"inv_pants_cloth_35","category":"gear"},{"type":"item","id":49992,"names":["Nibelung"],"icon":"inv_staff_99","category":"gear"},{"type":"item","id":50353,"names":["Dislodged Foreign Object"],"icon":"inv_misc_coin_18","category":"gear"},{"type":"item","id":50360,"names":["Phylactery of the Nameless Lich"],"icon":"inv_misc_ornatebox_03","category":"gear"},{"type":"item","id":50732,"names":["Bloodsurge, Kel'Thuzad's Blade of Agony","Bloodsurge, Kel’Thuzad’s Blade of Agony"],"icon":"inv_sword_154","category":"gear"},{"type":"item","id":54572,"names":["Charred Twilight Scale"],"icon":"inv_misc_rubysanctum4","category":"gear"},{"type":"spell","id":48113,"names":["Prayer of Mending"],"icon":"spell_holy_prayerofmendingtga","category":"healing"},{"type":"spell","id":48089,"names":["Circle of Healing"],"icon":"spell_holy_circleofrenewal","category":"healing"},{"type":"spell","id":48071,"names":["Flash Heal"],"icon":"spell_holy_flashheal","category":"healing"},{"type":"spell","id":48120,"names":["Binding Heal"],"icon":"spell_holy_blindingheal","category":"healing"},{"type":"spell","id":48072,"names":["Prayer of Healing"],"icon":"spell_holy_prayerofhealing02","category":"healing"},{"type":"spell","id":48063,"names":["Greater Heal"],"icon":"spell_holy_greaterheal","category":"healing"},{"type":"spell","id":48068,"names":["Renew"],"icon":"spell_holy_renew","category":"healing"},{"type":"spell","id":47788,"names":["Guardian Spirit"],"icon":"spell_holy_guardianspirit","category":"cooldown"},{"type":"spell","id":64843,"names":["Divine Hymn"],"icon":"spell_holy_divinehymn","category":"cooldown"},{"type":"spell","id":64901,"names":["Hymn of Hope"],"icon":"spell_holy_symbolofhope","category":"mana"},{"type":"spell","id":34433,"names":["Shadowfiend"],"icon":"spell_shadow_shadowfiend","category":"mana"},{"type":"spell","id":14751,"names":["Inner Focus"],"icon":"spell_frost_windwalkon","category":"cooldown"},{"type":"spell","id":63730,"names":["Serendipity"],"icon":"spell_holy_serendipity","category":"talent"},{"type":"spell","id":33150,"names":["Surge of Light"],"icon":"spell_holy_surgeoflight","category":"talent"},{"type":"spell","id":34860,"names":["Holy Concentration"],"icon":"spell_holy_fanaticism","category":"talent"},{"type":"spell","id":48066,"names":["Power Word: Shield"],"icon":"spell_holy_powerwordshield","category":"utility"},{"type":"spell","id":552,"names":["Abolish Disease"],"icon":"spell_nature_nullifydisease","category":"utility"},{"type":"spell","id":988,"names":["Dispel Magic"],"icon":"spell_holy_dispelmagic","category":"utility"},{"type":"spell","id":32375,"names":["Mass Dispel"],"icon":"spell_arcane_massdispel","category":"utility"},{"type":"spell","id":586,"names":["Fade"],"icon":"spell_magic_lesserinvisibilty","category":"utility"},{"type":"spell","id":6346,"names":["Fear Ward"],"icon":"spell_holy_excorcism","category":"utility"},{"type":"spell","id":48168,"names":["Inner Fire"],"icon":"spell_holy_innerfire","category":"buff"},{"type":"spell","id":48162,"names":["Prayer of Fortitude"],"icon":"spell_holy_prayeroffortitude","category":"buff"},{"type":"spell","id":48074,"names":["Prayer of Spirit"],"icon":"spell_holy_prayerofspirit","category":"buff"},{"type":"spell","id":48170,"names":["Prayer of Shadow Protection"],"icon":"spell_holy_prayerofshadowprotection","category":"buff"},{"type":"spell","id":48169,"names":["Power Word: Fortitude"],"icon":"spell_holy_wordfortitude","category":"buff"},{"type":"spell","id":48073,"names":["Divine Spirit"],"icon":"spell_holy_divinespirit","category":"buff"},{"type":"spell","id":48158,"names":["Shadow Word: Death"],"icon":"spell_shadow_demonicfortitude","category":"damage"},{"type":"spell","id":48125,"names":["Shadow Word: Pain"],"icon":"spell_shadow_shadowwordpain","category":"damage"},{"type":"spell","id":48160,"names":["Vampiric Touch"],"icon":"spell_holy_stoicism","category":"damage"},{"type":"spell","id":48300,"names":["Devouring Plague"],"icon":"spell_shadow_blackplague","category":"damage"},{"type":"spell","id":48127,"names":["Mind Blast"],"icon":"spell_shadow_unholyfrenzy","category":"damage"},{"type":"spell","id":48156,"names":["Mind Flay"],"icon":"spell_shadow_siphonmana","category":"damage"},{"type":"spell","id":53023,"names":["Mind Sear"],"icon":"spell_shadow_mindshear","category":"damage"},{"type":"spell","id":15473,"names":["Shadowform"],"icon":"spell_shadow_shadowform","category":"buff"},{"type":"spell","id":47585,"names":["Dispersion"],"icon":"spell_shadow_dispersion","category":"cooldown"},{"type":"spell","id":15286,"names":["Vampiric Embrace"],"icon":"spell_shadow_unsummonbuilding","category":"utility"},{"type":"spell","id":33193,"names":["Misery"],"icon":"spell_shadow_misery","category":"talent"},{"type":"spell","id":15258,"names":["Shadow Weaving"],"icon":"spell_shadow_blackplague","category":"talent"},{"type":"spell","id":47580,"names":["Pain and Suffering"],"icon":"spell_shadow_painandsuffering","category":"talent"},{"type":"spell","id":57669,"names":["Replenishment"],"icon":"spell_magic_managain","category":"buff"},{"type":"spell","id":15487,"names":["Silence"],"icon":"spell_shadow_impphaseshift","category":"utility"},{"type":"spell","id":1706,"names":["Levitate"],"icon":"spell_holy_layonhands","category":"utility"},{"type":"spell","id":48087,"names":["Lightwell"],"icon":"spell_holy_summonlightwell","category":"healing"},{"type":"spell","id":47898,"names":["Enchant Cloak - Greater Speed"],"icon":"trade_engraving","category":"enchant"},{"type":"spell","id":55642,"names":["Lightweave Embroidery"],"icon":"trade_tailoring","category":"enchant"},{"type":"spell","id":60692,"names":["Enchant Chest - Powerful Stats"],"icon":"trade_engraving","category":"enchant"},{"type":"spell","id":60767,"names":["Enchant Bracers - Superior Spellpower"],"icon":"trade_engraving","category":"enchant"},{"type":"spell","id":44592,"names":["Enchant Gloves - Exceptional Spellpower"],"icon":"trade_engraving","category":"enchant"},{"type":"spell","id":47901,"names":["Enchant Boots - Tuskarr's Vitality","Enchant Boots - Tuskarr’s Vitality"],"icon":"trade_engraving","category":"enchant"},{"type":"spell","id":60714,"names":["Enchant Weapon - Mighty Spellpower"],"icon":"trade_engraving","category":"enchant"},{"type":"spell","id":59630,"names":["Enchant Weapon - Black Magic"],"icon":"trade_engraving","category":"enchant"}];
+  const entities = new Map();
+  const entitiesByRef = new Map();
+  const phrases = [];
+
+  function normalize(value) {
+    return String(value || "").trim().toLowerCase().replace(/[’]/g, "'").replace(/\s+/g, " ");
+  }
+
+  rows.forEach(function (row) {
+    entitiesByRef.set(row.type + "=" + row.id, row);
+    row.names.forEach(function (name) {
+      entities.set(normalize(name), row);
+      phrases.push(name);
+    });
+  });
+
+  function wowheadUrl(entity) {
+    return "https://www.wowhead.com/wotlk/" + entity.type + "=" + entity.id;
+  }
+
+  function tooltipValue(entity) {
+    return entity.type + "=" + entity.id + "&domain=wotlk";
+  }
+
+  function decorateAnchor(anchor, entity) {
+    anchor.classList.add("wowhead-link");
+    anchor.setAttribute("data-wowhead", tooltipValue(entity));
+    if (!anchor.target) anchor.target = "_blank";
+    const rel = new Set((anchor.rel || "").split(/\s+/).filter(Boolean));
+    rel.add("noopener");
+    anchor.rel = Array.from(rel).join(" ");
+  }
+
+  function makeAnchor(text, entity) {
+    const anchor = document.createElement("a");
+    anchor.href = wowheadUrl(entity);
+    anchor.textContent = text;
+    decorateAnchor(anchor, entity);
+    return anchor;
+  }
+
+  function entityForNode(node) {
+    const requested = node.getAttribute("data-entity-name") || node.getAttribute("data-entity-icon") || node.textContent;
+    return entities.get(normalize(requested));
+  }
+
+  function decorateNamedEntities() {
+    const selector = [
+      ".game-entity", ".item-name", ".recipe-name", ".consumable-name", ".gem-name",
+      ".spell-name", ".ability-name", ".skill-name", ".talent-name", ".glyph-name", ".enchant-name"
+    ].join(",");
+
+    document.querySelectorAll(selector).forEach(function (node) {
+      if (node.hasAttribute("data-template-placeholder")) return;
+      const entity = entityForNode(node);
+      if (!entity || node.closest("code,pre,button,textarea")) return;
+      if (node.tagName === "A") {
+        decorateAnchor(node, entity);
+        return;
+      }
+      if (node.closest("a")) return;
+      const anchor = makeAnchor("", entity);
+      node.classList.forEach(function (className) { anchor.classList.add(className); });
+      while (node.firstChild) anchor.appendChild(node.firstChild);
+      node.appendChild(anchor);
+    });
+  }
+
+  function decorateExistingWowheadLinks() {
+    document.querySelectorAll('a[href*="wowhead.com/wotlk/"]').forEach(function (anchor) {
+      if (anchor.hasAttribute("data-wowhead")) return;
+      const match = anchor.href.match(/\/(item|spell|skill)=(\d+)/);
+      if (!match) return;
+      decorateAnchor(anchor, { type: match[1], id: Number(match[2]) });
+    });
+  }
+
+  function escapeRegExp(value) {
+    return value.replace(/[.*+?^$\{\}()|[\]\\]/g, "\\$&");
+  }
+
+  function linkPhrases() {
+    if (!phrases.length) return;
+    const ordered = Array.from(new Set(phrases)).sort(function (a, b) { return b.length - a.length; });
+    const matcher = new RegExp("(^|[^A-Za-z0-9'])((?:" + ordered.map(escapeRegExp).join("|") + "))(?=$|[^A-Za-z0-9'])", "gi");
+    const root = document.querySelector("main") || document.body;
+    const walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
+    const candidates = [];
+    let textNode;
+
+    while ((textNode = walker.nextNode())) {
+      const parent = textNode.parentElement;
+      if (!parent || !textNode.nodeValue.trim()) continue;
+      if (parent.closest("a,code,pre,script,style,textarea,button,iframe,nav,[data-no-entity-links],.game-entity,.item-name,.recipe-name,.consumable-name,.gem-name,.spell-name,.ability-name,.skill-name,.talent-name,.glyph-name,.enchant-name")) continue;
+      matcher.lastIndex = 0;
+      if (matcher.test(textNode.nodeValue)) candidates.push(textNode);
+    }
+
+    candidates.forEach(function (node) {
+      const fragment = document.createDocumentFragment();
+      let cursor = 0;
+      let match;
+      matcher.lastIndex = 0;
+
+      while ((match = matcher.exec(node.nodeValue))) {
+        const prefix = match[1];
+        const phrase = match[2];
+        const start = match.index + prefix.length;
+        if (start > cursor) fragment.appendChild(document.createTextNode(node.nodeValue.slice(cursor, start)));
+        const entity = entities.get(normalize(phrase));
+        fragment.appendChild(entity ? makeAnchor(phrase, entity) : document.createTextNode(phrase));
+        cursor = start + phrase.length;
+      }
+
+      if (cursor < node.nodeValue.length) fragment.appendChild(document.createTextNode(node.nodeValue.slice(cursor)));
+      node.parentNode.replaceChild(fragment, node);
+    });
+  }
+
+  function iconizeEntities() {
+    document.querySelectorAll("[data-entity-icon],.iconize-entity").forEach(function (node) {
+      if (node.querySelector(":scope > img.entity-icon")) return;
+      const entity = entityForNode(node);
+      if (!entity || !entity.icon) return;
+      const image = document.createElement("img");
+      image.className = node.getAttribute("data-icon-class") || "ability-icon entity-icon";
+      image.src = "https://wow.zamimg.com/images/wow/icons/large/" + entity.icon + ".jpg";
+      image.alt = "";
+      image.setAttribute("aria-hidden", "true");
+      image.onerror = function () { image.remove(); };
+      node.prepend(image);
+    });
+
+    if (document.body && document.body.dataset.entityIcons === "dense") {
+      document.querySelectorAll("a[data-wowhead].wowhead-link").forEach(function (anchor) {
+      if (anchor.querySelector(":scope > img")) return;
+      if (anchor.closest("h1,h2,h3,h4,summary,.chapter-card,.source-list,[data-no-entity-icons]")) return;
+      const match = anchor.getAttribute("data-wowhead").match(/^(item|spell|skill)=(\d+)/);
+      if (!match) return;
+      const entity = entitiesByRef.get(match[1] + "=" + match[2]);
+      if (!entity || !entity.icon) return;
+      const image = document.createElement("img");
+      image.className = "entity-link-icon";
+      image.src = "https://wow.zamimg.com/images/wow/icons/large/" + entity.icon + ".jpg";
+      image.alt = "";
+      image.setAttribute("aria-hidden", "true");
+      image.onerror = function () { image.remove(); };
+      anchor.prepend(image);
+      anchor.classList.add("has-entity-icon");
+      });
+    }
+  }
+
+  function loadWowheadTooltips() {
+    if (document.querySelector('script[data-priest-wowhead]')) return;
+    window.whTooltips = { colorLinks: false, iconizeLinks: false, renameLinks: false };
+    const script = document.createElement("script");
+    script.src = "https://wow.zamimg.com/js/tooltips.js";
+    script.async = true;
+    script.dataset.priestWowhead = "true";
+    document.head.appendChild(script);
+  }
+
+  function init() {
+    decorateNamedEntities();
+    linkPhrases();
+    decorateExistingWowheadLinks();
+    iconizeEntities();
+    loadWowheadTooltips();
+  }
+
+  if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", init, { once: true });
+  else init();
+}());
